2e0b6386f23822664beff110e032c28e13c721ae
wgr523 <wgr523@gmail.com>
2019-01-01 17:46:33 -0600

    update ReadJournal, use class HistoryStorage

--- examples/src/test/scala/hybrid/history/ReadJournal.scala
+++ examples/src/test/scala/hybrid/history/ReadJournal.scala
@@ -1,9 +1,16 @@
 package hybrid.history
 
 import java.io.File
-
+import com.typesafe.config.Config
+import net.ceedubs.ficus.Ficus._
+import net.ceedubs.ficus.readers.ArbitraryTypeReader._
+import net.ceedubs.ficus.readers.ValueReader
+import examples.hybrid.mining.{HybridMiningSettings, WalletSettings}
+import examples.hybrid.history.HistoryStorage
 import io.iohk.iodb.LSMStore
 import org.scalatest.PropSpec
+import scorex.core.settings.ScorexSettings
+import scorex.core.settings.ScorexSettings.readConfigFromPath
 
 class ReadJournal extends PropSpec{
   /**
@@ -12,12 +19,21 @@ class ReadJournal extends PropSpec{
     * TODO: understand the output.
     */
   {
-//    val dir = "/tmp/scorex/data/blockchain/blocks"
+    val userConfigPath = Some("examples/src/main/resources/settings.conf")
+    val config = readConfigFromPath(userConfigPath, "scorex")
+    val dataDir = config.getString("scorex.dataDir")
+    val miningSettings = config.as[HybridMiningSettings]("scorex.miner")
+    val blockStorage = new LSMStore(new File(dataDir + "/blocks"), maxJournalEntryCount = 10000)
+    //    val dir = "/tmp/scorex/data/blockchain/blocks"
 //    val dir = "/tmp/scorex/data/blockchain/state"
-    val dir = "/tmp/scorex/data/wallet/wallet.dat"
-    val iFile = new File(dir)
-    val blockStorage = new LSMStore(iFile, maxJournalEntryCount = 10000)
-    blockStorage.getAll((K,V) => println(K,V))
+//    val dir = "/tmp/scorex/data/wallet/wallet.dat"
+//    blockStorage.getAll((K,V) => println(K,V))
+
+//    val walletSettings = config.as[WalletSettings]("scorex.wallet")
+//    val scorexSettings = config.as[ScorexSettings]("scorex")
+
+    val historyStorage = new HistoryStorage(blockStorage, miningSettings)
+    //TODO
     blockStorage.close()
   }
 }
