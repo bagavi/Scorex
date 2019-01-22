b13fcf6e3f7528ece430b1977ae069ab7273baf1
wgr523 <wgr523@gmail.com>
2019-01-21 18:15:37 -0600

    logback.xml controls log level, now it's WARN. change PeersApiRoute and NetworkController in core and api.yaml. tiny change in settings

--- src/main/scala/scorex/core/network/NetworkController.scala
+++ src/main/scala/scorex/core/network/NetworkController.scala
@@ -105,6 +105,19 @@ class NetworkController(settings: NetworkSettings,
       log.info(s"Disconnected from ${peer.remote}")
       peer.handlerRef ! CloseConnection
 
+    case DisconnectFromAddr(peerAddress) =>
+      connectionForPeerAddress(peerAddress).map {
+        peer =>
+          log.warn(s"Disconnected from ${peer.remote}")
+          peerManagerRef ! RemovePeer(peer.remote)
+          peer.handlerRef ! CloseConnection
+          connections -= peer.remote
+          outgoing -= peer.remote
+          //TODO check this
+      }
+
+
+
     case Blacklist(peer) =>
       peer.handlerRef ! PeerConnectionHandler.ReceivableMessages.Blacklist
       // todo: the following message might become unnecessary if we refactor PeerManager to automatically
@@ -126,9 +139,9 @@ class NetworkController(settings: NetworkSettings,
       outgoing -= c.remoteAddress
       f.cause match {
         case Some(t) =>
-          log.info("Failed to connect to : " + c.remoteAddress, t)
+          log.warn("Failed to connect to : " + c.remoteAddress, t)
         case None =>
-          log.info("Failed to connect to : " + c.remoteAddress)
+          log.warn("Failed to connect to : " + c.remoteAddress)
       }
 
     case Terminated(ref) =>
@@ -391,6 +404,7 @@ object NetworkController {
     case object ShutdownNetwork
     case class ConnectTo(peer: PeerInfo)
     case class DisconnectFrom(peer: ConnectedPeer)
+    case class DisconnectFromAddr(peerAddress: InetSocketAddress)
     case class Blacklist(peer: ConnectedPeer)
     case object GetConnectedPeers
   }
