4b885626eee9c6eff784ee3b8b89d2d3f1058915
wgr523 <wgr523@gmail.com>
2019-02-06 15:59:57 -0600

    add invalid block tests to BlockTest

--- examples/src/test/scala/prism1/app/BlockTest.scala
+++ examples/src/test/scala/prism1/app/BlockTest.scala
@@ -24,44 +24,7 @@ import scala.sys.process._
 
 
 class BlockTest extends PropSpec with ScorexEncoding with ScorexLogging{
-
-  private val TransactionsPerBlock: Int = 50
-
-  private val getRequiredData: GetDataFromCurrentView[HybridHistory,
-    HBoxStoredState,
-    HBoxWallet,
-    SimpleBoxTransactionPrismMemPool,
-    PowMiningInfo] = {
-    val f: CurrentView[HybridHistory, HBoxStoredState, HBoxWallet, SimpleBoxTransactionPrismMemPool] => PowMiningInfo = {
-      view: CurrentView[HybridHistory, HBoxStoredState, HBoxWallet, SimpleBoxTransactionPrismMemPool] =>
-
-        val difficulty = view.history.powDifficulty
-        val bestPowBlock = view.history.bestPowBlock
-        // Pick transactions from the view's mempool
-        val txs = view.pool.take(TransactionsPerBlock).foldLeft(Seq[SimpleBoxTransactionPrism]()) { case (collected, tx) =>
-          val txNonces = tx.from.map{case (_ , nonce ) => nonce}
-          val collectedNonces = collected.flatMap(_.from).map{case (_ , nonce ) => nonce}
-          if (view.state.validate(tx).isSuccess &&
-            //ToDo: Vivek: I have edited this. To fix later.
-            //            tx.boxIdsToOpen.forall(id => !collected.flatMap(_.boxIdsToOpen).contains(id))) collected :+ tx
-            txNonces.forall(nonce => !collectedNonces.contains(nonce))) collected :+ tx
-          else collected
-        }
-
-        log.info(s"${txs.size} out of ${view.pool.size} transactions added.")
-        // TODO: fixme, What should we do if `view.vault.generateNewSecret().publicKeys` is empty?
-        @SuppressWarnings(Array("org.wartremover.warts.TraversableOps"))
-        val pubkey = view.vault.publicKeys.headOption getOrElse view.vault.generateNewSecret().publicKeys.head
-        PowMiningInfo(difficulty, bestPowBlock, pubkey, txs)
-    }
-    GetDataFromCurrentView[HybridHistory,
-      HBoxStoredState,
-      HBoxWallet,
-      SimpleBoxTransactionPrismMemPool,
-      PowMiningInfo](f)
-  }
-
-  private implicit val timeout = Timeout(5 seconds)
+  //private implicit val timeout = Timeout(5 seconds)
 
   import RunMainTest._
 
@@ -78,26 +41,27 @@ class BlockTest extends PropSpec with ScorexEncoding with ScorexLogging{
 
     var view = getNodeView(app.nodeViewHolderRef)
     var bestBlock: PowBlock = view._1.bestPowBlock
+    var balance = view._3.boxes().map(_.box).map(_.value.toLong).sum
 
     {
-      val future = app.nodeViewHolderRef ? getRequiredData
-      val pmi: PowMiningInfo = Await.result(future, timeout.duration).asInstanceOf[PowMiningInfo]
       @SuppressWarnings(Array("org.wartremover.warts.TraversableOps", "org.wartremover.warts.OptionPartial"))
       val recipient: PublicKey25519Proposition = PublicKey25519Proposition(PublicKey @@ encoder.decode("000000000000000a5177e290a0b1496751123eaef21992bcf5b20b9956bd1967").get)//a fake recipient.
       val fee: Long = 1L
       @SuppressWarnings(Array("org.wartremover.warts.TraversableOps", "org.wartremover.warts.OptionPartial"))
       val tx: SimpleBoxTransactionPrism = SimpleBoxTransactionPrism.create(view._3, Seq((recipient, Value @@ 2L)), fee).get
-      val pmiValid = pmi.copy(txs = Seq(tx))
+      @SuppressWarnings(Array("org.wartremover.warts.TraversableOps"))
+      val pubkey = view._3.publicKeys.headOption getOrElse view._3.generateNewSecret().publicKeys.head
+      val pmiValid = PowMiningInfo(view._1.powDifficulty, view._1.bestPowBlock, pubkey ,Seq(tx))
       app.miner ! pmiValid
       Thread.sleep(2000)
       view = getNodeView(app.nodeViewHolderRef)
       assert(view._1.bestPowBlock.parentId == bestBlock.id)
+      assert(balance + 83 - 2 - 1 == view._3.boxes().map(_.box).map(_.value.toLong).sum)
+      balance = view._3.boxes().map(_.box).map(_.value.toLong).sum
       bestBlock = view._1.bestPowBlock
     }
 
     {
-      val future = app.nodeViewHolderRef ? getRequiredData
-      val pmi: PowMiningInfo = Await.result(future, timeout.duration).asInstanceOf[PowMiningInfo]
       @SuppressWarnings(Array("org.wartremover.warts.TraversableOps", "org.wartremover.warts.OptionPartial"))
       val recipient: PublicKey25519Proposition = PublicKey25519Proposition(PublicKey @@ encoder.decode("000000000000000a5177e290a0b1496751123eaef21992bcf5b20b9956bd1967").get)//a fake recipient.
       val fee: Long = 1L
@@ -106,13 +70,36 @@ class BlockTest extends PropSpec with ScorexEncoding with ScorexLogging{
       val toInvalid: IndexedSeq[(PublicKey25519Proposition, Value)] =
         tx.to.map(p => (p._1, Value @@ (-p._2 - 1)))
       val txInvalid = tx.copy(to = toInvalid)
-      val pmiInvalid = pmi.copy(txs = Seq(txInvalid))
+      @SuppressWarnings(Array("org.wartremover.warts.TraversableOps"))
+      val pubkey = view._3.publicKeys.headOption getOrElse view._3.generateNewSecret().publicKeys.head
+      val pmiInvalid = PowMiningInfo(view._1.powDifficulty, view._1.bestPowBlock, pubkey ,Seq(txInvalid))
       app.miner ! pmiInvalid
       Thread.sleep(2000)
       view = getNodeView(app.nodeViewHolderRef)
-      assert(view._1.bestPowBlock.id == bestBlock.id)
+      assert(view._1.bestPowBlock.parentId == bestBlock.id)
+      assert(balance == view._3.boxes().map(_.box).map(_.value.toLong).sum)
+      balance = view._3.boxes().map(_.box).map(_.value.toLong).sum
+      bestBlock = view._1.bestPowBlock
     }
 
+    {
+      @SuppressWarnings(Array("org.wartremover.warts.TraversableOps", "org.wartremover.warts.OptionPartial"))
+      val recipient: PublicKey25519Proposition = PublicKey25519Proposition(PublicKey @@ encoder.decode("000000000000000a5177e290a0b1496751123eaef21992bcf5b20b9956bd1967").get)//a fake recipient.
+      val fee: Long = 1L
+      @SuppressWarnings(Array("org.wartremover.warts.TraversableOps", "org.wartremover.warts.OptionPartial"))
+      val tx: SimpleBoxTransactionPrism = SimpleBoxTransactionPrism.create(view._3, Seq((recipient, Value @@ 2L)), fee).get
+      val txInvalid = tx.copy(fee = 2L)
+      @SuppressWarnings(Array("org.wartremover.warts.TraversableOps"))
+      val pubkey = view._3.publicKeys.headOption getOrElse view._3.generateNewSecret().publicKeys.head
+      val pmiInvalid = PowMiningInfo(view._1.powDifficulty, view._1.bestPowBlock, pubkey ,Seq(txInvalid))
+      app.miner ! pmiInvalid
+      Thread.sleep(2000)
+      view = getNodeView(app.nodeViewHolderRef)
+      assert(view._1.bestPowBlock.parentId == bestBlock.id)
+      assert(balance == view._3.boxes().map(_.box).map(_.value.toLong).sum)
+      balance = view._3.boxes().map(_.box).map(_.value.toLong).sum
+      bestBlock = view._1.bestPowBlock
+    }
 
   }
 }
