d7d3e4a2a73be48d2266f0ff1f750880dde5fe80
wgr523 <wgr523@gmail.com>
2019-02-06 21:38:32 -0600

    finish BlockTest

--- examples/src/test/scala/bitcoin/app/BlockTest.scala
+++ examples/src/test/scala/bitcoin/app/BlockTest.scala
@@ -92,5 +92,84 @@ class BlockTest extends PropSpec with ScorexEncoding with ScorexLogging{
     }
 
   }
+
+  property("Block containing 2 conflicting tx get in the chain. But the txs are rejected") {
+    "src/main/resources/testbench/ConfigTestGenerator.sh topology.txt" !
+
+    val app = new BitcoinApp("src/main/resources/testbench/testsettings1.conf")
+    app.run()
+    Thread.sleep(1000)
+    app.miner ! StartMining
+    Thread.sleep(2000)
+    app.miner ! StopMining
+    Thread.sleep(1000)
+
+    var view = getNodeView(app.nodeViewHolderRef)
+    var bestBlock: PowBlock = view._1.bestPowBlock
+    var balance = view._3.boxes().map(_.box).map(_.value.toLong).sum
+
+    @SuppressWarnings(Array("org.wartremover.warts.TraversableOps", "org.wartremover.warts.OptionPartial"))
+    val recipient: PublicKey25519Proposition = PublicKey25519Proposition(PublicKey @@ encoder.decode("000000000000000a5177e290a0b1496751123eaef21992bcf5b20b9956bd1967").get)//a fake recipient.
+    val fee: Long = 1L
+    @SuppressWarnings(Array("org.wartremover.warts.TraversableOps", "org.wartremover.warts.OptionPartial"))
+    val tx: SimpleBoxTransactionBitcoin = SimpleBoxTransactionBitcoin.create(view._3, Seq((recipient, Value @@ 2L)), fee).get
+    @SuppressWarnings(Array("org.wartremover.warts.TraversableOps"))
+    val pubkey = view._3.publicKeys.headOption getOrElse view._3.generateNewSecret().publicKeys.head
+    val txInvalid = tx.copy(timestamp = System.currentTimeMillis())
+    @SuppressWarnings(Array("org.wartremover.warts.TraversableOps"))
+    val pmiInvalid = PowMiningInfo(view._1.powDifficulty, view._1.bestPowBlock, pubkey ,Seq(tx, txInvalid))
+    app.miner ! pmiInvalid
+    Thread.sleep(2000)
+    view = getNodeView(app.nodeViewHolderRef)
+    assert(view._1.bestPowBlock.parentId == bestBlock.id)
+    assert(balance == view._3.boxes().map(_.box).map(_.value.toLong).sum)
+    balance = view._3.boxes().map(_.box).map(_.value.toLong).sum
+    bestBlock = view._1.bestPowBlock
+
+  }
+
+  property("2 Blocks containing 2 conflicting tx get in the chain. But the second tx is rejected") {
+    "src/main/resources/testbench/ConfigTestGenerator.sh topology.txt" !
+
+    val app = new BitcoinApp("src/main/resources/testbench/testsettings1.conf")
+    app.run()
+    Thread.sleep(1000)
+    app.miner ! StartMining
+    Thread.sleep(2000)
+    app.miner ! StopMining
+    Thread.sleep(1000)
+
+    var view = getNodeView(app.nodeViewHolderRef)
+    var bestBlock: PowBlock = view._1.bestPowBlock
+    var balance = view._3.boxes().map(_.box).map(_.value.toLong).sum
+
+    @SuppressWarnings(Array("org.wartremover.warts.TraversableOps", "org.wartremover.warts.OptionPartial"))
+    val recipient: PublicKey25519Proposition = PublicKey25519Proposition(PublicKey @@ encoder.decode("000000000000000a5177e290a0b1496751123eaef21992bcf5b20b9956bd1967").get)//a fake recipient.
+    val fee: Long = 1L
+    @SuppressWarnings(Array("org.wartremover.warts.TraversableOps", "org.wartremover.warts.OptionPartial"))
+    val tx: SimpleBoxTransactionBitcoin = SimpleBoxTransactionBitcoin.create(view._3, Seq((recipient, Value @@ 2L)), fee).get
+    @SuppressWarnings(Array("org.wartremover.warts.TraversableOps"))
+    val pubkey = view._3.publicKeys.headOption getOrElse view._3.generateNewSecret().publicKeys.head
+    val pmiValid = PowMiningInfo(view._1.powDifficulty, view._1.bestPowBlock, pubkey ,Seq(tx))
+    app.miner ! pmiValid
+    Thread.sleep(2000)
+    view = getNodeView(app.nodeViewHolderRef)
+    assert(view._1.bestPowBlock.parentId == bestBlock.id)
+    assert(balance + 83 - 2 - 1 == view._3.boxes().map(_.box).map(_.value.toLong).sum)
+    balance = view._3.boxes().map(_.box).map(_.value.toLong).sum
+    bestBlock = view._1.bestPowBlock
+
+    val txInvalid = tx.copy(timestamp = System.currentTimeMillis())
+    @SuppressWarnings(Array("org.wartremover.warts.TraversableOps"))
+    val pmiInvalid = PowMiningInfo(view._1.powDifficulty, view._1.bestPowBlock, pubkey ,Seq(txInvalid))
+    app.miner ! pmiInvalid
+    Thread.sleep(2000)
+    view = getNodeView(app.nodeViewHolderRef)
+    assert(view._1.bestPowBlock.parentId == bestBlock.id)
+    assert(balance == view._3.boxes().map(_.box).map(_.value.toLong).sum)
+    balance = view._3.boxes().map(_.box).map(_.value.toLong).sum
+    bestBlock = view._1.bestPowBlock
+
+  }
 }
 
